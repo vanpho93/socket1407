b171844fd2d9f1c869cd2b7d6340a51214d03d7b
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-05 20:38:13 +0700

    xong

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.render('home'));
+server.listen(3000, () => console.log('Server started!'));
+
+io.on('connection', (socket) => console.log(socket.id));
