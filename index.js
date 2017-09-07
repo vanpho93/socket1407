3a715159f57aee21f5aa2f4dadb28c313ed11797
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-07 19:59:12 +0700

    requires

--- index.js
+++ index.js
@@ -11,8 +11,14 @@ app.use(express.static('public'));
 app.get('/', (req, res) => res.render('home'));
 server.listen(3000, () => console.log('Server started!'));
 
+const arrUsername = [];
+
 io.on('connection', socket => {
     socket.on('CLIENT_SEND_MESSAGE', message => {
         io.emit('SERVER_SEND_MESSAGE', message);
     });
 });
+
+// Click sign in -> Gui username len server, server add username vao mang arrUsername 
+// Server dap tra da dang ky thanh cong, client an hien divs
+
