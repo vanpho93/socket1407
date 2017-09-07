264521ad713ed7de635d700a9bbe299af6e21b29
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-07 19:40:02 +0700

    init 2

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ app.use(express.static('public'));
 app.get('/', (req, res) => res.render('home'));
 server.listen(3000, () => console.log('Server started!'));
 
-// gui -- lang nghe
-
 io.on('connection', socket => {
-    // setInterval(() => socket.emit('SERVER_SEND_MESSAGE', Math.random()), 2000);
     socket.on('CLIENT_SEND_MESSAGE', message => {
         io.emit('SERVER_SEND_MESSAGE', message);
     });
